c8dc0a1fa7df4d3b0a7fc008a4844e5d791d981e
Kirill Spirin <spiridon729@gmail.com>
2024-09-16 23:56:09 +0300

    Task1Hiber

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Иван", "Иванов", (byte) 22);
+        userService.saveUser("Петр", "Петров", (byte) 23);
+        userService.saveUser("Иван", "Сидоров", (byte) 43);
+        userService.saveUser("Петр", "Иванов", (byte) 18);
+        userService.getAllUsers();
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import javax.persistence.PersistenceException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +19,85 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createSQLQuery(
+                    "CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY, " +
+                            "name VARCHAR(50)," +
+                            "lastname VARCHAR(50)," +
+                            "age SMALLINT)");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            System.out.println("Ошибка создания таблицы " + e.getMessage());
+        }
     }
-
-    @Override
-    public void dropUsersTable() {
-
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
-
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Пользователь с id " + id + " не может быть удален, т.к. не существует. \n" + e.getMessage());
+        } catch (HibernateException e) {
+            System.out.println("Ошибка удаления пользователя по id " + e.getMessage());
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("FROM User");
+            users = query.getResultList();
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка при попытке достать всех пользователей  " + e.getMessage());
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE FROM User");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка очистки таблицы  " + e.getMessage());
+        }
+    }
 
+
+    @Override
+    public void dropUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("DROP TABLE IF EXISTS person");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка удаления таблицы  " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.saveOrUpdate(user);
+            transaction.commit();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (HibernateException e) {
+            System.out.println("Ошибка сохранения пользователя " + e.getMessage());
+        }
     }
 }
